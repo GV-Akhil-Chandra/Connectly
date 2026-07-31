0ae46005ac326e3cee9515adcd007c7be60923bf
Akhil Chandra <gvakhilca1@gmail.com>
2026-08-01 01:07:00 +0530

    Security: connections-service with RequestInterceptor, FeignClientRequestInterceptor and context holder

---
+++ connection-service/src/main/java/com/Connectly/connection_service/Authentication/RequestInterceptor.java
@@ -0,0 +1,27 @@
+package com.Connectly.connection_service.Authentication;
+
+import com.Connectly.connection_service.Authentication.AuthContextHolder;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class RequestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        Fetching the userID from the request header and setting it in thread local
+        Long userId = Long.valueOf(request.getHeader("X-User-Id"));
+        AuthContextHolder.setCurrentUserId(userId);
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        // clearing thread local
+        AuthContextHolder.clear();
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
---
+++ connection-service/src/main/java/com/Connectly/connection_service/Authentication/WebMVCConfigurerImpl.java
@@ -0,0 +1,22 @@
+package com.Connectly.connection_service.Authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfigurerImpl implements WebMvcConfigurer {
+    @Autowired
+    private RequestInterceptor requestInterceptor;
+
+    /**
+     * Registering newly created "RequestInterceptor" to WebMVC
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestInterceptor);
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
