825acf951c4f00c0057a3c0e6bcdd81890b2c3f3
Akhil Chandra <gvakhilca1@gmail.com>
2026-07-31 23:21:02 +0530

    Security: posts-service authContextHolder and requestInterceptor

--- posts-service/src/main/java/com/Connectly/posts_service/Controller/PostController.java
+++ posts-service/src/main/java/com/Connectly/posts_service/Controller/PostController.java
@@ -1,5 +1,6 @@
 package com.Connectly.posts_service.Controller;
 
+import com.Connectly.posts_service.Authentication.AuthContextHolder;
 import com.Connectly.posts_service.DTO.PostCreationDTO;
 import com.Connectly.posts_service.DTO.PostDTO;
 import com.Connectly.posts_service.Service.PostService;
@@ -21,12 +22,14 @@ public class PostController {
     @PostMapping("/create-post")
     public ResponseEntity<PostDTO> createCost(@RequestBody PostCreationDTO postCreationDTO,
                                               HttpServletRequest httpServletRequest){
-        PostDTO postDTO = postService.createPost(postCreationDTO, 1L);
+        // using AuthContextHolder as we intercept the request and fetch the userID and store it in thread local
+        PostDTO postDTO = postService.createPost(postCreationDTO, AuthContextHolder.getCurrentUserId());
         return new ResponseEntity<>(postDTO, HttpStatus.CREATED);
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<PostDTO> getPostById(@PathVariable(name = "postId") Long postId){
+    public ResponseEntity<PostDTO> getPostById(@PathVariable(name = "postId") Long postId,
+                                               @RequestHeader("X-User-Id") Long userId){
         PostDTO postDTO = postService.findById(postId);
         return new ResponseEntity<>(postDTO, HttpStatus.FOUND);
     }
